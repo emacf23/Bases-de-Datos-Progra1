1a07cef2f99841aff1656ee35336bb17ef7a352e
Emmanuel Castro <emacf23@outlook.com>
2019-08-12 12:20:01 -0600

    Diay no se la verdad

---
+++ Bases-de-Datos-Progra1/Bases-de-Datos-Progra1/db/DatabasePrueba/dbo/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [CustomerId] INT           NOT NULL,
+    [username]   NVARCHAR (50) NOT NULL,
+    [PASSWORD]   NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC)
+);
+
